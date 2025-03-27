fa2094a876e02ff894f5cef4b466a26f114ea3a9
Jan Schulz (LXPLUS) <jan.gerrit.schulz@cern.ch>
2025-03-27 09:40:51 +0100

    Expose doublet building cut parameters of Patatrack pixel tracking

--- RecoTracker/PixelSeeding/plugins/alpaka/CAHitNtupletGenerator.cc
+++ RecoTracker/PixelSeeding/plugins/alpaka/CAHitNtupletGenerator.cc
@@ -61,6 +61,12 @@ namespace ALPAKA_ACCELERATOR_NAMESPACE {
       desc.add<bool>("doSharedHitCut", true)->setComment("Sharing hit nTuples cleaning");
       desc.add<bool>("dupPassThrough", false)->setComment("Do not reject duplicate");
       desc.add<bool>("useSimpleTripletCleaner", true)->setComment("use alternate implementation");
+
+      desc.add<int>("minYsizeB1", 25);
+      desc.add<int>("minYsizeB2", 15);
+      desc.add<int>("cellMaxDYSize12", 12)->setComment("Maximum cluster size difference for B1/B2");
+      desc.add<int>("cellMaxDYSize", 10)->setComment("Maximum cluster size difference");
+      desc.add<int>("cellMaxDYPred", 20)->setComment("Maximum cluster size difference prediction");
     }
 
     AlgoParams makeCommonParams(edm::ParameterSet const& cfg) {
@@ -150,6 +156,11 @@ namespace ALPAKA_ACCELERATOR_NAMESPACE {
                                       cfg.getParameter<bool>("idealConditions"),
                                       (float)cfg.getParameter<double>("cellZ0Cut"),
                                       (float)cfg.getParameter<double>("cellPtCut"),
+                                      cfg.getParameter<int>("minYsizeB1"),
+                                      cfg.getParameter<int>("minYsizeB2"),
+                                      cfg.getParameter<int>("cellMaxDYSize12"),
+                                      cfg.getParameter<int>("cellMaxDYSize"),
+                                      cfg.getParameter<int>("cellMaxDYPred"),
                                       cfg.getParameter<std::vector<int>>("phiCuts")};
     }
 
--- RecoTracker/PixelSeeding/plugins/alpaka/CAPixelDoubletsAlgos.h
+++ RecoTracker/PixelSeeding/plugins/alpaka/CAPixelDoubletsAlgos.h
@@ -52,24 +52,41 @@ namespace ALPAKA_ACCELERATOR_NAMESPACE::caPixelDoublets {
               const bool idealConditions,
               const float z0Cut,
               const float ptCut,
+              const int minYsizeB1,
+              const int minYsizeB2,
+              const int maxDYSize12,
+              const int maxDYSize,
+              const int maxDYPred,
               const std::vector<int>& phiCutsV)
         : doClusterCut_(doClusterCut),
           doZ0Cut_(doZ0Cut),
           doPtCut_(doPtCut),
           idealConditions_(idealConditions),
           z0Cut_(z0Cut),
-          ptCut_(ptCut) {
+          ptCut_(ptCut),
+          minYsizeB1_(minYsizeB1),
+          minYsizeB2_(minYsizeB2),
+          maxDYSize12_(maxDYSize12),
+          maxDYSize_(maxDYSize),
+          maxDYPred_(maxDYPred) {
       assert(phiCutsV.size() == TrackerTraits::nPairs);
       std::copy(phiCutsV.begin(), phiCutsV.end(), &phiCuts[0]);
     }
 
-    bool doClusterCut_;
-    bool doZ0Cut_;
-    bool doPtCut_;
-    bool idealConditions_;  //this is actually not used by phase2
+    const bool doClusterCut_;
+    const bool doZ0Cut_;
+    const bool doPtCut_;
+    const bool idealConditions_;  //this is actually not used by phase2
 
-    float z0Cut_;  //FIXME: check if could be const now
-    float ptCut_;
+    const float z0Cut_;  //FIXME: check if could be const now
+    const float ptCut_;
+
+    const int minYsizeB1_;
+    const int minYsizeB2_;
+
+    const int maxDYSize12_;
+    const int maxDYSize_;
+    const int maxDYPred_;
 
     int phiCuts[T::nPairs];
 
@@ -98,10 +115,10 @@ namespace ALPAKA_ACCELERATOR_NAMESPACE::caPixelDoublets {
 
       if (not innerBarrel and not onlyBarrel)
         return false;
-      auto dy = innerB1 ? T::maxDYsize12 : T::maxDYsize;
+      auto dy = innerB1 ? maxDYSize12_ : maxDYSize_;
 
       return onlyBarrel ? so > 0 && std::abs(so - mes) > dy
-                        : innerBarrel && std::abs(mes - int(std::abs(dz / dr) * T::dzdrFact + 0.5f)) > T::maxDYPred;
+                        : innerBarrel && std::abs(mes - int(std::abs(dz / dr) * T::dzdrFact + 0.5f)) > maxDYPred_;
     }
 
     template <typename TAcc>
@@ -119,11 +136,11 @@ namespace ALPAKA_ACCELERATOR_NAMESPACE::caPixelDoublets {
       auto mes = (!innerB1) || isOuterLadder ? hh[i].clusterSizeY() : -1;
 
       if (innerB1)  // B1
-        if (mes > 0 && mes < T::minYsizeB1)
+        if (mes > 0 && mes < minYsizeB1_)
           return true;                                                                 // only long cluster  (5*8)
       bool innerB2 = (mi >= T::last_bpix1_detIndex) && (mi < T::last_bpix2_detIndex);  //FIXME number
       if (innerB2)                                                                     // B2 and F1
-        if (mes > 0 && mes < T::minYsizeB2)
+        if (mes > 0 && mes < minYsizeB2_)
           return true;
 
       return false;
