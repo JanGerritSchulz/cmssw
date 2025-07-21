dda38f91b1481e0871b5819b6e3fb5ffec5f2e7b
Jan Schulz <jan.gerrit.schulz@cern.ch>
2025-07-21 08:37:08 +0200

    Update PixelTrackNtuplizer to newer version with OT RecHits

--- Validation/RecoTrack/plugins/PixelTrackNtuplizer.cc
+++ Validation/RecoTrack/plugins/PixelTrackNtuplizer.cc
@@ -4,6 +4,39 @@
 #include "TTree.h"
 #include "TFile.h"
 
+/* ################################################################################
+To be used like this:
+---------------------------------
+
+1. Create your cmsconfig with cmsDriver.py, e.g.:
+```
+cmsDriver.py step2 -s L1P2GT,HLT:75e33,VALIDATION:@hltValidation --conditions auto:phase2_realistic_T33 \
+--datatier GEN-SIM-DIGI-RAW,DQMIO -n 10 --eventcontent FEVTDEBUGHLT,DQMIO --geometry ExtendedRun4D110 \
+--era Phase2C17I13M9 --procModifier alpaka --filein file:step2.root --fileout file:step3.root --no_exec \
+--process HLTX
+```
+
+2. Modify the line with the validation step from this:
+```
+process.validation_step = cms.EndPath(process.hltvalidation)
+```
+to this:
+```
+process.TFileService = cms.Service('TFileService', fileName=cms.string('./pixelTrackNtuples.root'))
+process.load('Validation.RecoTrack.pixelTrackNtuplizer_cfi')
+process.validation_step = cms.EndPath(
+    process.hltvalidation +
+    process.pixelTrackNtuplizer)
+```
+
+3. Run the config as usual:
+```
+cmsRun step2_L1P2GT_HLT_VALIDATION.py
+```
+
+4. A file named "pixelTrackNtuples.root" is created.
+*/
+
 // user include files
 // #include "FWCore/Framework/interface/Event.h"
 #include "FWCore/Framework/interface/ESHandle.h"
@@ -15,6 +48,8 @@
 #include "FWCore/Framework/interface/Frameworkfwd.h"
 #include "FWCore/Framework/interface/Event.h"
 #include "Geometry/Records/interface/TrackerTopologyRcd.h"
+#include "Geometry/Records/interface/TrackerDigiGeometryRecord.h"
+#include "Geometry/TrackerGeometryBuilder/interface/TrackerGeometry.h"
 
 #include "DataFormats/TrackerCommon/interface/TrackerTopology.h"
 #include "FWCore/Framework/interface/MakerMacros.h"
@@ -30,6 +65,7 @@
 #include "DataFormats/TrackerRecHit2D/interface/SiPixelRecHitCollection.h"
 #include "DataFormats/TrackerRecHit2D/interface/SiPixelRecHit.h"
 #include "DataFormats/TrackerRecHit2D/interface/SiPixelRecHitFwd.h"
+#include "DataFormats/TrackerRecHit2D/interface/Phase2TrackerRecHit1D.h"
 
 #include "SimTracker/TrackerHitAssociation/interface/ClusterTPAssociation.h"
 #include "SimDataFormats/PileupSummaryInfo/interface/PileupSummaryInfo.h"
@@ -92,17 +128,24 @@ private:
   TrackingParticleSelector trackingParticleSelector;
 
   const TrackerTopology* trackerTopology_ = nullptr;
+  const TrackerGeometry* trackerGeometry_ = nullptr;
   const edm::ESGetToken<TrackerTopology, TrackerTopologyRcd> topology_getToken_;
+  const edm::ESGetToken<TrackerGeometry, TrackerDigiGeometryRecord> geometry_getToken_;
   const edm::EDGetTokenT<SiPixelRecHitCollection> pixelRecHits_getToken_;
+  const edm::EDGetTokenT<Phase2TrackerRecHit1DCollectionNew> otRecHits_getToken_;
   const edm::EDGetTokenT<edm::View<reco::Track>> tracks_getToken_;
   const edm::EDGetTokenT<reco::TrackToTrackingParticleAssociator> trackAssociatorToken_;
   const edm::EDGetTokenT<TrackingParticleCollection> trackingParticleToken_;
   const edm::EDGetTokenT<ClusterTPAssociation> clusterTPAssociation_getToken_;
+  const edm::EDGetTokenT<reco::BeamSpot> beamSpot_getToken_;
 
   TTree* output_tree_;
   // general event information
 
   edm::EventNumber_t ev_event_;
+  float beamspot_x_;
+  float beamspot_y_;
+  float beamspot_z_;
 
   // TrackingParticles
   std::vector<float> trackingParticle_eta_;
@@ -112,6 +155,7 @@ private:
   std::vector<bool> trackingParticle_isMissed_;
   std::vector<bool> trackingParticle_isSignal_;
   std::vector<bool> trackingParticle_isIntime_;
+  std::vector<int> trackingParticle_nLayers_;
   std::vector<int> trackingParticle_nHits_;
   std::vector<int> trackingParticle_hitIds_;
   std::vector<std::pair<TrackingParticleRef::key_type, std::vector<int>>>
@@ -130,21 +174,27 @@ private:
   std::vector<float> track_eta_;
   std::vector<float> track_phi_;
   std::vector<float> track_pt_;
+  std::vector<float> track_tip_;
+  std::vector<float> track_zip_;
   std::vector<float> track_nchi2_;
   std::vector<float> track_ndof_;
+  std::vector<int> track_nLayers_;
   std::vector<int> track_nHits_;
   std::vector<int> track_hitIds_;
   std::vector<bool> track_isFake_;
   std::vector<bool> track_isSignal_;
+  std::vector<int> track_trackingParticle_;
 };
 
 void PixelTrackNtuplizer::fillDescriptions(edm::ConfigurationDescriptions& descriptions) {
   edm::ParameterSetDescription desc;
   desc.add<edm::InputTag>("trackSrc", edm::InputTag("hltPhase2PixelTracks"));
   desc.add<edm::InputTag>("pixelRecHitSrc", edm::InputTag("hltSiPixelRecHits"));
+  desc.add<edm::InputTag>("otRecHitSrc", edm::InputTag("hltSiPhase2RecHits"));
   desc.add<edm::InputTag>("trackingParticles", edm::InputTag("mix", "MergedTrackTruth"));
   desc.addUntracked<edm::InputTag>("trackAssociator", edm::InputTag("hltTrackAssociatorByHits"));
   desc.add<edm::InputTag>("clusterTPAssociationSrc", edm::InputTag("hltTPClusterProducer"));
+  desc.add<edm::InputTag>("beamSpotSrc", edm::InputTag("hltOnlineBeamSpot"));
 
   // parameter set for the selection of TrackingParticles that will be used for SimHitDoublets
   edm::ParameterSetDescription descTPSelector;
@@ -170,14 +220,17 @@ void PixelTrackNtuplizer::fillDescriptions(edm::ConfigurationDescriptions& descr
 
 PixelTrackNtuplizer::PixelTrackNtuplizer(const edm::ParameterSet& pSet)
     : topology_getToken_(esConsumes<TrackerTopology, TrackerTopologyRcd>()),
+      geometry_getToken_(esConsumes<TrackerGeometry, TrackerDigiGeometryRecord>()),
       pixelRecHits_getToken_(consumes(pSet.getParameter<edm::InputTag>("pixelRecHitSrc"))),
+      otRecHits_getToken_(consumes(pSet.getParameter<edm::InputTag>("otRecHitSrc"))),
       tracks_getToken_(consumes<edm::View<reco::Track>>(pSet.getParameter<edm::InputTag>("trackSrc"))),
       trackAssociatorToken_(consumes<reco::TrackToTrackingParticleAssociator>(
           pSet.getUntrackedParameter<edm::InputTag>("trackAssociator"))),
       trackingParticleToken_(
           consumes<TrackingParticleCollection>(pSet.getParameter<edm::InputTag>("trackingParticles"))),
       clusterTPAssociation_getToken_(
-          consumes<ClusterTPAssociation>(pSet.getParameter<edm::InputTag>("clusterTPAssociationSrc"))) {
+          consumes<ClusterTPAssociation>(pSet.getParameter<edm::InputTag>("clusterTPAssociationSrc"))),
+      beamSpot_getToken_(consumes(pSet.getParameter<edm::InputTag>("beamSpotSrc"))) {
   // initialize the selector for TrackingParticles used to create SimDoublets
   const edm::ParameterSet& pSetTPSel = pSet.getParameter<edm::ParameterSet>("TrackingParticleSelectionConfig");
   trackingParticleSelector = TrackingParticleSelector(pSetTPSel.getParameter<double>("ptMin"),
@@ -203,8 +256,17 @@ void PixelTrackNtuplizer::analyze(const edm::Event& iEvent, const edm::EventSetu
   // clear all vectors
   clearVectors();
 
-  // get tracker topology
+  // get tracker topology and geometry
   trackerTopology_ = &iSetup.getData(topology_getToken_);
+  trackerGeometry_ = &iSetup.getData(geometry_getToken_);
+
+  // get beamspot from the event
+  edm::Handle<reco::BeamSpot> beamSpot;
+  iEvent.getByToken(beamSpot_getToken_, beamSpot);
+  if (!beamSpot.isValid()) {
+    return;
+  }
+
   // get tracks
   edm::Handle<View<reco::Track>> trackCollectionHandle;
   if (!iEvent.getByToken(tracks_getToken_, trackCollectionHandle)) {
@@ -224,6 +286,13 @@ void PixelTrackNtuplizer::analyze(const edm::Event& iEvent, const edm::EventSetu
   edm::Handle<SiPixelRecHitCollection> hits;
   iEvent.getByToken(pixelRecHits_getToken_, hits);
   if (!hits.isValid()) {
+    return;
+  }
+
+  // get the OT RecHit collection from the event
+  edm::Handle<Phase2TrackerRecHit1DCollectionNew> hitsOT;
+  iEvent.getByToken(otRecHits_getToken_, hitsOT);
+  if (!hitsOT.isValid()) {
     return;
   }
 
@@ -323,9 +392,66 @@ void PixelTrackNtuplizer::analyze(const edm::Event& iEvent, const edm::EventSetu
     detId2lastHitIdMap.insert({detId, hitId});
   }  // end loop over pixel RecHit collections of the different pixel modules
 
+  // loop over OT RecHit collections of the different OT modules
+  for (const auto& detSet : *hitsOT) {
+    // get detector Id
+    uint detId = detSet.detId();
+      DetId detIdObject(detId);
+
+    // get layerId of the OT (barrel: 1,2,3,...; endcap: 101, 201, 102, 202)
+    uint layerIdOT = trackerTopology_->getOTLayerNumber(detId);
+
+    // only use the RecHits if the module is in the accepted range of layers and one of the Phase 2 PS, p-sensor
+    if ((layerIdOT <= 3) &&
+        (trackerGeometry_->getDetectorType(detIdObject) == TrackerGeometry::ModuleType::Ph2PSP)) {
+      // layerId corrected by number of pixel layers
+      uint layerId = layerIdOT + 27;
+      // put the current hitId in the map
+      detId2firstHitIdMap.insert({detId, hitId});
+
+      // loop over RecHits
+      for (auto const& hit : detSet) {
+        // fill hit properties
+        GlobalPoint hitPosition = hit.globalPosition();
+        recHit_x_.push_back(hitPosition.x());
+        recHit_y_.push_back(hitPosition.y());
+        recHit_z_.push_back(hitPosition.z());
+        recHit_detId_.push_back(detId);
+        recHit_layerId_.push_back(layerId);
+
+        // find associated TrackingParticles
+        auto range = clusterTPAssociation.equal_range(OmniClusterRef(hit.cluster()));
+
+        // if the RecHit has associated TrackingParticles
+        int tpId = -1;
+        if (range.first != range.second) {
+          for (auto assocTrackingParticleIter = range.first; assocTrackingParticleIter != range.second;
+               assocTrackingParticleIter++) {
+            const TrackingParticleRef assocTrackingParticle = (assocTrackingParticleIter->second);
+
+            // loop over collection and find the one of the associated TrackingParticle
+            for (size_t j{0}; j < trackingParticle_hitIds_Vector_.size(); j++) {
+              auto& tpHitsPair = trackingParticle_hitIds_Vector_.at(j);
+              if (assocTrackingParticle.key() == tpHitsPair.first) {
+                tpHitsPair.second.push_back(hitId);
+                tpId = j;
+                break;
+              }
+            }
+          }
+        }
+        recHit_trackingParticle_.push_back(tpId);
+        hitId++;
+      }  // end loop over RecHits
+      // put the current hitId in the map
+      detId2lastHitIdMap.insert({detId, hitId});
+    }
+  }  // end loop over OT RecHit collections of the different pixel modules
+
   // loop over Tracks
-  for (auto const& track : trackRefs) {
+  for (std::unordered_set<uint> layerSet{}; auto const& track : trackRefs) {
     // check if the track is associated
+    int tpId = -1;
     bool isFake{true};
     bool isSignal{false};
     auto foundTP = recSimColl.find(track);
@@ -334,16 +460,31 @@ void PixelTrackNtuplizer::analyze(const edm::Event& iEvent, const edm::EventSetu
       if (!tp.empty()) {
         isFake = false;
         isSignal = (tp[0].first->eventId().bunchCrossing() == 0 && tp[0].first->eventId().event() == 0);
+
+        // find the correct TP
+        for (size_t j{0}; auto const& tpHitsPair : trackingParticle_hitIds_Vector_) {
+          if (tp[0].first.key() == tpHitsPair.first) {
+            tpId = j;
+            break;
+          }
+          j++;
+        }
       }
     }
     track_isFake_.push_back(isFake);
     track_isSignal_.push_back(isSignal);
+    track_trackingParticle_.push_back(tpId);
     track_nHits_.push_back(track->recHitsSize());
     track_eta_.push_back(track->eta());
     track_phi_.push_back(track->phi());
     track_pt_.push_back(track->pt());
+    track_tip_.push_back(track->dxy());
+    track_zip_.push_back(track->dz());
     track_nchi2_.push_back(track->normalizedChi2());
     track_ndof_.push_back(track->ndof());
+
+    // reset layerSet to be filled with RecHit layers of the current track
+    layerSet.clear();
 
     // loop over the RecHits of that Track
     for (size_t j{0}; j < track->recHitsSize(); j++) {
@@ -354,22 +495,33 @@ void PixelTrackNtuplizer::analyze(const edm::Event& iEvent, const edm::EventSetu
         if ((std::abs(recHit_x_.at(k) - globalPos.x()) < 1e-4) && (std::abs(recHit_y_.at(k) - globalPos.y()) < 1e-4) &&
             (std::abs(recHit_z_.at(k) - globalPos.z()) < 1e-4)) {
           track_hitIds_.push_back(k);
+          layerSet.insert(recHit_layerId_.at(k));
           break;
         }
       }
     }
+    // fill the number of layers of the track
+    track_nLayers_.push_back(layerSet.size());
   }
 
   // finalize the TrackingParticle hitIds
-  for (auto const& tpHitsPair : trackingParticle_hitIds_Vector_) {
+  for (std::unordered_set<uint> layerSet{}; auto const& tpHitsPair : trackingParticle_hitIds_Vector_) {
     trackingParticle_nHits_.push_back(tpHitsPair.second.size());
+
+    layerSet.clear();
     for (auto const hitId_ : tpHitsPair.second) {
       trackingParticle_hitIds_.push_back(hitId_);
+      layerSet.insert(recHit_layerId_.at(hitId_));
     }
+    // fill the number of layers of the trackingParticle
+    trackingParticle_nLayers_.push_back(layerSet.size());
   }
 
   // set general event information
   ev_event_ = iEvent.id().event();
+  beamspot_x_ = beamSpot->x0();
+  beamspot_y_ = beamSpot->y0();
+  beamspot_z_ = beamSpot->z0();
 
   // fill the tree
   output_tree_->Fill();
@@ -380,6 +532,9 @@ void PixelTrackNtuplizer::beginJob() {
   output_tree_ = fs->make<TTree>("PixelTracks", "Pixel Track Validation TTree");
 
   output_tree_->Branch("event", &ev_event_);
+  output_tree_->Branch("beamspot_x", &beamspot_x_);
+  output_tree_->Branch("beamspot_y", &beamspot_y_);
+  output_tree_->Branch("beamspot_z", &beamspot_z_);
   output_tree_->Branch("trackingParticle_eta", &trackingParticle_eta_);
   output_tree_->Branch("trackingParticle_phi", &trackingParticle_phi_);
   output_tree_->Branch("trackingParticle_pt", &trackingParticle_pt_);
@@ -387,6 +542,7 @@ void PixelTrackNtuplizer::beginJob() {
   output_tree_->Branch("trackingParticle_isMissed", &trackingParticle_isMissed_);
   output_tree_->Branch("trackingParticle_isSignal", &trackingParticle_isSignal_);
   output_tree_->Branch("trackingParticle_isIntime", &trackingParticle_isIntime_);
+  output_tree_->Branch("trackingParticle_nLayers", &trackingParticle_nLayers_);
   output_tree_->Branch("trackingParticle_nHits", &trackingParticle_nHits_);
   output_tree_->Branch("trackingParticle_hitIds", &trackingParticle_hitIds_);
   output_tree_->Branch("recHit_x", &recHit_x_);
@@ -398,12 +554,16 @@ void PixelTrackNtuplizer::beginJob() {
   output_tree_->Branch("track_eta", &track_eta_);
   output_tree_->Branch("track_phi", &track_phi_);
   output_tree_->Branch("track_pt", &track_pt_);
+  output_tree_->Branch("track_tip", &track_tip_);
+  output_tree_->Branch("track_zip", &track_zip_);
   output_tree_->Branch("track_nchi2", &track_nchi2_);
   output_tree_->Branch("track_ndof", &track_ndof_);
+  output_tree_->Branch("track_nLayers", &track_nLayers_);
   output_tree_->Branch("track_nHits", &track_nHits_);
   output_tree_->Branch("track_hitIds", &track_hitIds_);
   output_tree_->Branch("track_isFake", &track_isFake_);
   output_tree_->Branch("track_isSignal", &track_isSignal_);
+  output_tree_->Branch("track_trackingParticle", &track_trackingParticle_);
 }
 
 void PixelTrackNtuplizer::endJob() {}
@@ -416,6 +576,7 @@ void PixelTrackNtuplizer::clearVectors() {
   trackingParticle_isMissed_.clear();
   trackingParticle_isSignal_.clear();
   trackingParticle_isIntime_.clear();
+  trackingParticle_nLayers_.clear();
   trackingParticle_nHits_.clear();
   trackingParticle_hitIds_.clear();
   recHit_x_.clear();
@@ -427,12 +588,16 @@ void PixelTrackNtuplizer::clearVectors() {
   track_eta_.clear();
   track_phi_.clear();
   track_pt_.clear();
+  track_tip_.clear();
+  track_zip_.clear();
   track_nchi2_.clear();
   track_ndof_.clear();
+  track_nLayers_.clear();
   track_nHits_.clear();
   track_hitIds_.clear();
   track_isFake_.clear();
   track_isSignal_.clear();
+  track_trackingParticle_.clear();
 
   trackingParticle_hitIds_Vector_.clear();
   recHit_keysVector_.clear();
