7312517e86a48f15ee98e589f6ea9df97337c162
Jan Schulz <jan.gerrit.schulz@cern.ch>
2026-02-18 16:40:36 +0100

    Fix size of tupleMultiplicity container in Patatrack

--- RecoTracker/PixelSeeding/plugins/alpaka/CAHitNtupletGeneratorKernels.dev.cc
+++ RecoTracker/PixelSeeding/plugins/alpaka/CAHitNtupletGeneratorKernels.dev.cc
@@ -147,12 +147,14 @@ namespace ALPAKA_ACCELERATOR_NAMESPACE {
     device_tupleMultiplicityStorage_ =
         cms::alpakatools::make_device_buffer<GenericContainerStorage[]>(queue, maxTuples);
     device_tupleMultiplicityOffsets_ =
-        cms::alpakatools::make_device_buffer<GenericContainerOffsets[]>(queue, TrackerTraits::maxHitsOnTrack + 1);
+        cms::alpakatools::make_device_buffer<GenericContainerOffsets[]>(queue, TrackerTraits::maxHitsOnTrack + 2);
     device_tupleMultiplicityView_ = {
         device_tupleMultiplicity_->data(),
         device_tupleMultiplicityOffsets_->data(),
         device_tupleMultiplicityStorage_->data(),
-        int(TrackerTraits::maxHitsOnTrack + 1),  //TODO: this could become configurable with some work
+        // this has to be +2 instead of +1 because you want all values from 0 to maxHitsOnTrack to be valid keys
+        // (N+1 values) + the extra +1 for the Container definition
+        int(TrackerTraits::maxHitsOnTrack + 2),
         int(maxTuples)};
     TupleMultiplicity::template launchZero<Acc1D>(device_tupleMultiplicityView_, queue);
 
