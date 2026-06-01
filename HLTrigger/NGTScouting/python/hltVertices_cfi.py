fe74683a7f6518d62c65bf023f8009349347997b
Jan Schulz <jan.gerrit.schulz@cern.ch>
2026-06-01 15:10:45 +0200

    Add Secondary Vertices to NGT scouting NanoAOD

--- HLTrigger/NGTScouting/python/hltVertices_cfi.py
+++ HLTrigger/NGTScouting/python/hltVertices_cfi.py
@@ -1,21 +1,29 @@
 import FWCore.ParameterSet.Config as cms
 from PhysicsTools.NanoAOD.common_cff import *
+from PhysicsTools.NanoAOD.simpleSecondaryVertexFlatTableProducer_cfi import simpleSecondaryVertexFlatTableProducer
 
 hltVertexTable = cms.EDProducer("HLTVertexTableProducer",
                                 skipNonExistingSrc = cms.bool(True),
                                 pvSrc = cms.InputTag("hltOfflinePrimaryVertices"),
+                                svSrc = cms.InputTag("hltDeepInclusiveMergedVerticesPF"),
                                 goodPvCut = cms.string("!isFake && ndof >= 4.0 && abs(z) <= 24.0 && abs(position.Rho) <= 2.0"), 
+                                goodSvCut = cms.string(""), 
                                 pfSrc = cms.InputTag("hltParticleFlowTmp"),
+                                doSVs = cms.bool(True),
                                 dlenMin = cms.double(0),
                                 dlenSigMin = cms.double(3),
-                                pvName = cms.string("hltPrimaryVertex"))
+                                pvName = cms.string("hltPrimaryVertex"),
+                                svName = cms.string("hltSecondaryVertex"),
+                                svDoc  = cms.string("secondary vertices from IVF algorithm"))
 
 hltPixelVertexTable = cms.EDProducer("HLTVertexTableProducer",
                                      skipNonExistingSrc = cms.bool(True),
                                      pvSrc = cms.InputTag("hltPhase2PixelVertices"),
                                      goodPvCut = cms.string(""),
+                                     goodSvCut = cms.string(""), 
                                      usePF = cms.bool(False), # use directly the tracks from PV fit 
                                      pfSrc = cms.InputTag(""),
+                                     doSVs = cms.bool(False),
                                      dlenMin = cms.double(0),
                                      dlenSigMin = cms.double(3),
                                      pvName = cms.string("hltPixelVertex"))
