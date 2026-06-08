d0a461d77472c1c4910acc498ad60edb25dcda25
Jan Schulz <jan.gerrit.schulz@cern.ch>
2026-06-08 14:00:04 +0200

    Add SimVertices to NGTScouting Nano

--- HLTrigger/NGTScouting/python/hltVertices_cfi.py
+++ HLTrigger/NGTScouting/python/hltVertices_cfi.py
@@ -28,20 +28,16 @@ hltPixelVertexTable = cms.EDProducer("HLTVertexTableProducer",
                                      dlenSigMin = cms.double(3),
                                      pvName = cms.string("hltPixelVertex"))
 
-hltSecondaryVertexTable = cms.EDProducer("SimpleSecondaryVertexFlatTableProducer",
-                                         skipNonExistingSrc = cms.bool(True),
-                                         src = cms.InputTag("hltDeepInclusiveMergedVerticesPF"),
-                                         name = cms.string("hltSecondaryVertex"),
+
+hltSimVertexTable = cms.EDProducer("SimpleTrackingVertexFlatTableProducer",
+                                         skipNonExistingSrc = cms.bool(False),
+                                         src = cms.InputTag("mix", "MergedTrackTruth"),
+                                         name = cms.string("hltSimVertex"),
                                          extension = cms.bool(False),
-                                         variables = cms.PSet(P4Vars,
+                                         variables = cms.PSet(
                                                               x   = Var("position().x()", float, doc = "secondary vertex X position, in cm",precision=10),
                                                               y   = Var("position().y()", float, doc = "secondary vertex Y position, in cm",precision=10),
                                                               z   = Var("position().z()", float, doc = "secondary vertex Z position, in cm",precision=14),
-                                                              ndof    = Var("vertexNdof()", float, doc = "number of degrees of freedom",precision=8),
-                                                              chi2    = Var("vertexNormalizedChi2()", float, doc = "reduced chi2, i.e. chi/ndof",precision=8),
-                                                              ntracks = Var("numberOfDaughters()", "uint8", doc = "number of tracks"),
+                                                              ntracks = Var("nDaughterTracks()", "uint8", doc = "number of tracks"),
                                                               ),
                                          )
-
-hltSecondaryVertexTable.variables.pt.precision=10
-hltSecondaryVertexTable.variables.phi.precision=12
